e1c3b6bf1c8bcf2c4d83463ef6cf22a64b7f11c6
PythonNut <PythonNut@users.noreply.github.com>
2019-06-03 22:35:12 -0700

    Switch to gradient penalties

--- dfa_pytorch.py
+++ dfa_pytorch.py
@@ -19,12 +19,13 @@ class DFA(nn.Module):
         self.f /= self.f.sum()
         self.f.requires_grad = True
 
+
     def forward(self, s):
         q = torch.zeros(self.n)
         q[0] = 1
         for sym in s:
             q = self.delta[sym] @ q
-        return q.dot(self.f)
+        return self.f @ q
 
 train = [
     ([1,1,1,1,1,1,1], 1),
@@ -33,18 +34,30 @@ train = [
     ([1,1,1,1], 0),
     ([1,1,1], 1),
     ([1,1],0),
-    ([1], 1)
+    ([1], 1),
+
+    ([1,1,1,1,0,1,1,1], 1),
+    ([1,1,1,0,1,1,1], 0),
+    ([1,1,0,0,0,0,1,1,0,1], 1),
+    ([1,1,1,1], 0),
+    ([1,1,1], 1),
+    ([1,1],0),
+    ([1,0,0,0,0], 1)
 ]
 
-model = DFA(2, 2)
-optim = torch.optim.Adam(model.parameters(), lr=0.1)
+def main():
+    n, s = 2, 2
+    model = DFA(n, s)
+    optim = torch.optim.SGD(model.parameters(), lr=0.001)
 
-for epoch in range(1000):
-    for x, y in train:
-        model.zero_grad()
-        y_pred = model(x)
-        loss = - y*y_pred.log() - (1-y)*(1-y_pred).log()
-        loss.backward()
-        optim.step()
-        model.normalize()
-        print(model.delta)
+    for epoch in range(5000):
+        for x, y in train:
+            model.zero_grad()
+            y_pred = model(x)
+            reg = torch.cat((model.delta.permute((0, 2, 1)).contiguous().view(n * s, -1), model.f.unsqueeze(0)))
+            reg_loss1 = (reg.sum(1) - 1).pow(2).sum()
+            reg_loss2 = reg.clamp(max=0).abs().pow(2).sum() + (reg-1).clamp(min=0).pow(2).sum()
+            loss = - y*y_pred.log() - (1-y)*(1-y_pred).log() + 100 * reg_loss1 + 100 * reg_loss2
+            loss.backward()
+            optim.step()
+            print(model.f)
