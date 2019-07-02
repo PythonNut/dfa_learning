7650a3b58bd29dafa94e4ccd3088030bd4a16471
PythonNut <PythonNut@users.noreply.github.com>
2019-07-02 14:12:23 -0700

    Update SDP code

--- dfa_sdp.jl
+++ dfa_sdp.jl
@@ -21,9 +21,9 @@ function array_mod(a, base, mod)
 end
 
 
-# function dfa(x)
-#     return Int(array_mod(x, 2, d) == 0) + Int(array_mod(x, 2, d) == 4)
-# end
+function dfa(x)
+    return Int(array_mod(x, 2, d) == 0) + Int(array_mod(x, 2, d) == 4)
+end
 
 # function dfa(x)
 #    if length(x) < 3
@@ -33,27 +33,30 @@ end
 #    return Int(x[end-2] == 1)
 # end
 
-function dfa(x)
-   switches = 0
-   for i in 2:length(x)
-       if x[i] != x[i - 1]
-           switches += 1
-       end
-   end
-   return switches % 3 == 0
-end
+# function dfa(x)
+#    switches = 0
+#    for i in 2:length(x)
+#        if x[i] != x[i - 1]
+#            switches += 1
+#        end
+#    end
+#    return switches % 3 == 0
+# end
 
 Hf = hcat([[dfa(vcat(p, s)) for s in P] for p in P]...)'
 Hf0 = hcat([[dfa(vcat(p, [0], s)) for s in P] for p in P]...)'
 Hf1 = hcat([[dfa(vcat(p, [1], s)) for s in P] for p in P]...)'
 
 
-mask = randn(size(Hf)) .> 0.5
 
 model = Model(with_optimizer(ProxSDP.Optimizer, log_verbose=true, full_eig_decomp=true))
 
-n, m = size(Hf)
-M = Hf
+# M = Hf
+
+M = vcat(Hf, Hf0, Hf1)
+mask = randn(size(M)) .> 1.5
+
+n, m = size(M)
 
 @variable(model, W1[1:n, 1:n])
 @variable(model, W2[1:m, 1:m])
@@ -61,6 +64,7 @@ M = Hf
 
 @constraint(model, [W1 X; X' W2] in PSDCone())
 @constraint(model, mask .* (X - M) .== 0)
+@constraint(model, 1 .>= X .>= 0)
 
 @objective(model, MOI.MIN_SENSE, tr(W1) + tr(W2))
 
