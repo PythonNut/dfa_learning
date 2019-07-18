4763823d5a033ffa476572b586f16b801e0baab6
PythonNut <PythonNut@users.noreply.github.com>
2019-07-18 12:07:43 -0700

    Add BFS ordering check

--- dfa_sat.py
+++ dfa_sat.py
@@ -6,6 +6,7 @@ import itertools as it
 import numpy as np
 import operator as op
 from functools import reduce, partial
+from queue import Queue
 
 def read_dct(fname):
     results = []
@@ -187,6 +188,66 @@ def min_dfa_setup_model_pop(M, V, train, prefixes_f, prefixes_r, G, sigma, h):
 
     a = V.dispense((h, n))
     b = V.dispense((n, h))
+    # x = V.dispense((n, h))
+    y = V.dispense((s, h, h))
+    z = V.dispense((h,))
+
+    q = 0
+
+    for v in VV:
+        M.add_clause((-b[v, 0],))
+        M.add_clause((-a[h-1, v],))
+
+        # for i in range(h):
+        #     cnf = CardEnc.equals(lits=(x[v, i], a[i, v], b[v, i]), encoding=EncType.pairwise)
+        #     M.append_formula(cnf)
+
+        for i in range(h-1):
+            M.add_clause((-a[i+1, v], a[i, v]))
+
+            M.add_clause((a[i, v], b[v, i+1]))
+            M.add_clause((-a[i, v], -b[v, i+1]))
+
+            M.add_clause((-a[i, v], a[i, q]))
+
+    for pi, p in enumerate(prefixes_f):
+        for l in range(len(sigma)):
+            c = tuple([*p, sigma[l]])
+            if c in prefixes_r:
+                ci = prefixes_r[c]
+                for i in range(h):
+                    for j in range(h):
+                        M.add_clause((y[l, i, j], a[i, pi], b[pi, i], a[j, ci], b[ci, j]))
+                        M.add_clause((-y[l, i, j], a[i, pi], b[pi, i], -a[j, ci]))
+                        M.add_clause((-y[l, i, j], a[i, pi], b[pi, i], -b[ci, j]))
+
+    for l, i in it.product(range(len(sigma)), range(h)):
+        cnf = CardEnc.equals(lits=[y[l, i, j] for j in range(h)], encoding=EncType.pairwise)
+        M.append_formula(cnf)
+
+    for (seq, accept) in train:
+        pi = prefixes_r[seq]
+        for i in range(h):
+            if accept == 1:
+                M.add_clause((a[i, pi], b[pi, i], z[i]))
+            else:
+                M.add_clause((a[i, pi], b[pi, i], -z[i]))
+
+    for u, v in EE:
+        for i in range(h):
+            M.add_clause((a[i, u], b[u, i], a[i, v], b[v, i]))
+
+    return M
+
+def min_dfa_setup_model_pop2(M, V, train, prefixes_f, prefixes_r, G, sigma, h):
+    VV = G.nodes
+    EE = G.edges
+
+    n = len(VV)
+    s = len(sigma)
+
+    a = V.dispense((h, n))
+    b = V.dispense((n, h))
     x = V.dispense((n, h))
     y = V.dispense((s, h, h))
     z = V.dispense((h,))
@@ -194,6 +255,9 @@ def min_dfa_setup_model_pop(M, V, train, prefixes_f, prefixes_r, G, sigma, h):
     q = 0
 
     for v in VV:
+        cnf = CardEnc.equals(lits=[x[v, i] for i in range(h)], encoding=EncType.pairwise)
+        M.append_formula(cnf)
+
         M.add_clause((-b[v, 0],))
         M.add_clause((-a[h-1, v],))
 
@@ -292,6 +356,62 @@ def break_dfa_symmetry_bfs(M, V, sigma, prefixes_r, h):
     return M
 
 def break_dfa_symmetry_bfs_pop(M, V, sigma, prefixes_r, h):
+    a, b, y, z = V.variables
+    s = len(sigma)
+
+    p = V.dispense((h, h))
+    t = V.dispense((h, h))
+
+    epsilon_i = prefixes_r[()]
+    assert epsilon_i == 0
+    M.add_clause((-a[h-1, epsilon_i],))
+    M.add_clause((-b[epsilon_i, h-1],))
+
+    for j, i in it.combinations(range(h), 2):
+        M.add_clause(it.chain((-t[i, j],), (y[l, i, j] for l in range(s))))
+        for l in range(s):
+            M.add_clause((-y[l, i, j], t[i, j]))
+
+        M.add_clause((-p[j, i], t[i, j]))
+        M.add_clause(it.chain(
+            (-t[i, j], p[j, i]),
+            (t[k, j] for k in range(i+1, h))
+        ))
+
+    for j, i, k in it.combinations(range(h), 3):
+        M.add_clause((-p[j, i], -t[k, j]))
+
+    for j in range(h-1):
+        M.add_clause(p[j, i] for i in range(j+1, h))
+
+    for j, i, k in it.combinations(range(1, h), 3):
+        M.add_clause((-p[j, i], -p[j-1, k]))
+
+    if s == 2:
+        for j, i in it.combinations(range(1, h), 2):
+            M.add_clause((-p[j, i], -p[j-1, i], y[0, i, j]))
+    else:
+        assert False
+        m = V.dispense((s, h, h))
+
+        for i, j in it.combinations(range(h), 2):
+            for l in range(s):
+                M.add_clause((-m[l, i, j], y[l, i ,j]))
+                M.add_clause(it.chain(
+                    (-y[l, i, j], m[l, i, j]),
+                    (y[k, i, j] for k in range(l))
+                ))
+
+            for k, n in it.combinations(range(s), 2):
+                M.add_clause((-m[n, i, j], -y[k, i, j]))
+
+        for i, j in it.combinations(range(h-1), 2):
+            for k, n in it.combinations(range(s), 2):
+                M.add_clause((-p[j, i], -p[j+1, i], -m[n, i, j], -m[k, i, j+1]))
+
+    return M
+
+def break_dfa_symmetry_bfs_pop2(M, V, sigma, prefixes_r, h):
     _, _, x, y, z = V.variables
     s = len(sigma)
 
@@ -324,7 +444,7 @@ def break_dfa_symmetry_bfs_pop(M, V, sigma, prefixes_r, h):
 
     if s == 2:
         for j, i in it.combinations(range(1, h), 2):
-            M.add_clause((-p[j, i], -p[j-1, i], y[1, i, j]))
+            M.add_clause((-p[j, i], -p[j-1, i], y[0, i, j]))
     else:
         assert False
         m = V.dispense((s, h, h))
@@ -476,6 +596,14 @@ def extract_dfa(M, V, sigma, prefixes_r):
     return q1, z.astype(int), A
 
 def extract_dfa_pop(M, V, sigma, prefixes_r):
+    a, b, y, z, *_ = V.unflatten(M.get_model())
+    x = 1 - (a.transpose() + b)
+    epsilon_i = prefixes_r[()]
+    A = [y[l, :, :].astype(int) for l in range(len(sigma))]
+    q1 = x[epsilon_i, :].astype(int)
+    return q1, z.astype(int), A
+
+def extract_dfa_pop2(M, V, sigma, prefixes_r):
     _, _, x, y, z, *_ = V.unflatten(M.get_model())
     epsilon_i = prefixes_r[()]
     A = [y[l, :, :].astype(int) for l in range(len(sigma))]
@@ -491,8 +619,53 @@ def dfa_eval(dfa, seq, sigma):
         (qinf,)
     ))
 
+def transition_matrix_to_delta(A):
+    k = A[0].shape[0]
+    delta = {}
+    for i in range(k):
+        delta[i] = [np.where(a[i, :] == 1)[0][0] for a in A]
+    return delta
+
+def is_sorted(l):
+    return all(a <= b for a, b in zip(l, l[1:]))
+
+def bfs_dfa(q1, qinf, A):
+    k = len(q1)
+    delta = transition_matrix_to_delta(A)
+    visited = np.zeros(k, np.bool)
+    q = Queue()
+    q1i = np.where(q1)[0][0]
+    visited[q1i] = True
+    result = []
+
+    q.put(q1i)
+    result.append(q1i)
+    while not q.empty():
+        v = q.get()
+        for w in delta[v]:
+            if not visited[w]:
+                visited[w] = True
+                q.put(w)
+                result.append(w)
+
+    return result
+
+def print_noam(q1, qinf, A, fname='noam.txt'):
+    with open(fname, 'w') as f:
+        q1i = np.where(q1)[0][0]
+        delta = transition_matrix_to_delta(A)
+        f.writelines(['#states\n'])
+        f.writelines([f's{i}\n' for i in range(len(q1))])
+        f.writelines([f'#initial\ns{q1i}\n'])
+        f.writelines(['#accepting\n'])
+        f.writelines([f's{i}\n' for i in np.where(qinf)[0]])
+        f.writelines(['#alphabet\n'])
+        f.writelines([f'{i}\n' for i in range(len(A))])
+        f.writelines(['#transitions\n'])
+        f.writelines([f's{i}:{j}>s{o}\n' for i, l in delta.items() for j,o in enumerate(l)])
+
 if __name__ == '__main__':
-    train = read_dct('dcts/dfa_12_try_7.dct')
+    train = read_dct('dcts/dfa_12_try_8.dct')
     prefixes_f, prefixes_r, suffixes_f, suffixes_r = enumerate_fixes(train)
     G  = build_distinguishability_graph(train, prefixes_r, suffixes_r)
 
@@ -505,11 +678,11 @@ if __name__ == '__main__':
 
     while True:
         V = VariableDispenser()
-        M = Maplesat(use_timer=True) # Glucose4(use_timer=True)
+        M = MapleChrono(use_timer=True) # Glucose4(use_timer=True)
 
         print(f'Building problem for {states} states')
-        min_dfa_setup_model_pop(M, V, train, prefixes_f, prefixes_r, G, sigma, states)
-        break_dfa_symmetry_bfs_pop(M, V, sigma, prefixes_r, states)
+        min_dfa_setup_model(M, V, train, prefixes_f, prefixes_r, G, sigma, states)
+        break_dfa_symmetry_bfs(M, V, sigma, prefixes_r, states)
 
         print(f'Starting solver: {M.nof_vars()} vars, {M.nof_clauses()} clauses')
 
@@ -530,6 +703,7 @@ if __name__ == '__main__':
     print(f'Found solution with {states} states!')
     print(f'Took {total_time:.4f} seconds')
 
-    dfa = extract_dfa_pop(M, V, sigma, prefixes_r)
+    dfa = extract_dfa(M, V, sigma, prefixes_r)
 
+    assert is_sorted(bfs_dfa(*dfa)[::1])
     assert all([dfa_eval(dfa, seq, range(2)) == accept for seq, accept in train])
