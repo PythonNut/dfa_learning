b748719c897d5d44019d753cca678e6a2d779a82
krzxr <rongkyle@gmail.com>
2019-08-26 23:14:15 -0700

    modify update

--- dfa_search.cpp
+++ dfa_search.cpp
@@ -14,6 +14,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+set<int> set_state(int prefix,int state, int *prefix2state,unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p, stack<set<int>> prefix_table_t, bool trace);
+set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p,stack<set<int>> prefix_table_t,set<int> potential, map< int,set< int>> prefix2constrain,vector<vector<int>> constrain_content, bool trace);
 bool cmp(string a, string b) {
    	if(a.size()!=b.size())
 		return a.size()<b.size();
@@ -31,17 +33,17 @@ int main(){
 	//read inputs from file. might want to optimize to take prefix set and suffix set in here.
 	char file_path[] = "./dcts/dfa_12_try_7.dct";
 	freopen(file_path,"r",stdin);
-	unsigned int train_size,alsize;
+	int train_size,alsize;
 	scanf("%d %d",&train_size,&alsize);
 	string train_string[train_size];
-	unsigned int train_label[train_size];
-	for(unsigned int i = 0; i<train_size;i++ ){
-		unsigned int label, string_length;
+	int train_label[train_size];
+	for(int i = 0; i<train_size;i++ ){
+		int label, string_length;
 		scanf("%d %d",&label,&string_length);
 		string s;
-		unsigned int count =0;
+		int count =0;
 		while(count<string_length){
-			unsigned int d;
+			int d;
 			scanf(" %d",&d);
 			s=s+to_string(d);
 			count++;
@@ -50,13 +52,12 @@ int main(){
 		train_string[i] = s;
 		train_label[i]=label;
 	}
-
 	//enumerate fixes. generate the prefix and suffix sets
 	set<string, decltype(&cmp)> prefixes(&cmp);
 	set<string>  suffixes;
 	string prefix, suffix;
-	for(unsigned int i=0;i<train_size; i++){
-		for(unsigned int j=0;j< train_string[i].length()+1; j++){
+	for(int i=0;i<train_size; i++){
+		for(int j=0;j< train_string[i].length()+1; j++){
 			prefix=train_string[i].substr(0,j);
 			suffix=train_string[i].substr(j,train_string[i].length());
 			prefixes.insert(prefix);
@@ -65,9 +66,9 @@ int main(){
 	}
 	
 	// build dictionaries
-	unordered_map<string,unsigned int> prefixes_map, suffixes_map;
-	unordered_map<unsigned int,string>prefixes_indexes;
-	unsigned int count=0;
+	unordered_map<string,int> prefixes_map, suffixes_map;
+	unordered_map<int,string>prefixes_indexes;
+	int count=0;
 	for(set<string>::iterator i=prefixes.begin();i!=prefixes.end(); i++){
 		prefixes_indexes[count]=*i;
 		prefixes_map[*i]=count++;
@@ -93,21 +94,18 @@ int main(){
 	// the range is 0-prefix_size-1, prefix_size-state-1+prefix_size 
 	// dictionary: 0 is unstated, 1 is stated
 	// set includes possible number of states
-	unordered_map<unsigned int,set<unsigned int>> different_group, constrain2prefix,prefix2constrain;
-	// cannot change the above to vector bc we need it to be indexed properly
-	vector<vector<int>> constrain_content;	
-	vector<vector<char>> constrain_op;	
+	unordered_map<int,set<int>> different_group;
 
 
 	// build distinguishability map
-	unordered_map<unsigned int,unordered_map<unsigned int,set<unsigned int>>> dmap;
-	unordered_map<unsigned int,unsigned int> map_label;
-	for(unsigned int i=0;i<train_size; i++){
-		for(unsigned int j=0;j< train_string[i].length()+1; j++){
+	unordered_map<int,unordered_map<int,set<int>>> dmap;
+	unordered_map<int,int> map_label;
+	for(int i=0;i<train_size; i++){
+		for(int j=0;j< train_string[i].length()+1; j++){
 			prefix=train_string[i].substr(0,j);
 			suffix=train_string[i].substr(j,train_string[i].length());
-			unsigned int pi = prefixes_map[prefix];
-			unsigned int si = suffixes_map[suffix];
+			int pi = prefixes_map[prefix];
+			int si = suffixes_map[suffix];
 			
 			if(j==train_string[i].length()+1){
 				map_label[pi]=train_label[i];
@@ -121,85 +119,77 @@ int main(){
 	}
 	
 	//for condition 2
-	unordered_map<unsigned int,set<tuple<unsigned int,unsigned int>>> pre_dmap;
-	unordered_map<string,unsigned int> suffixes_map2;
-
-	unordered_map<unsigned int,string> suffixes_map20;
-	unsigned int k=0;
-	unsigned int r=0;
-	for(auto i=prefixes.begin();i!=prefixes.end(); i++){
-		for(unsigned int j=0;j< (*i).size()+1; j++){
+	unordered_map<int,set<tuple<int,int>>> pre_dmap;
+	//long prefix id, short prefix id
+	unordered_map<string,int> suffixes_map2;
+	unordered_map<int,string> suffixes_map20;
+	int suf=0;
+	int long_pre=0;
+	for(set<string>::iterator i=prefixes.begin();i!=prefixes.end(); i++){
+		for( int j=0;j< (*i).size()+1; j++){
 			prefix=(*i).substr(0,j);
 			suffix=(*i).substr(j,(*i).size());
 			if(prefixes_map.find(prefix)!=prefixes_map.end()){
-				cout<<"IN!  "<<prefix<<"  "<<*i<<"\n";
-				unsigned int pi = prefixes_map[prefix];	
+				int pi = prefixes_map[prefix];	
 				if(suffixes_map2.find(suffix)==suffixes_map2.end()){
-					suffixes_map20[k]=suffix;
-					suffixes_map2[suffix]=k++;
+					suffixes_map20[suf]=suffix;
+					suffixes_map2[suffix]=suf++;
 				}
-				unsigned int si = suffixes_map2[suffix];
-				tuple<unsigned int,unsigned int> t=make_tuple(r,pi);
-				pre_dmap[si].insert(t);
+				int si = suffixes_map2[suffix];
+				pre_dmap[si].insert(make_tuple(long_pre,pi));
 			}
 			else
 				continue;
 			
-			}
-		r++;
+		}
+		long_pre++;
 	}
-		
-	for(unordered_map<unsigned int,set<tuple<unsigned int,unsigned int>>>::iterator i=pre_dmap.begin();i!=pre_dmap.end(); i++){
+	printf("finish condition 2 prep\n");
+
+	map<int,set<int>> prefix2constrain; //constrain2prefix;
+	// cannot change the above to vector bc we need it to be indexed properly
+	vector<vector<int>>constrain_content;	
+	for(unordered_map< int,set<tuple< int, int>>>::iterator i=pre_dmap.begin();i!=pre_dmap.end(); i++){
 		
 		//cout<<"yesss   "<<suffixes_map20[i->first]<<"\n";
 		for(auto j=i->second.begin();j!=i->second.end();j++){
 			//cout<<get<0>(*j)<<"  no "<<prefixes_indexes[get<0>(*j)]<<" hi  "<<prefixes_indexes[get<1>(*j)]<<"   \n";
-			for(auto k=j+1;k!=i->second.end();k++){
-				vector<int> c_c, c_o;
-				int c_id=constrain_op.size();
+			auto k2=j;
+			for(auto k=k2++;k!=i->second.end();k++){
+				vector<int> c;
+				int c_id=constrain_content.size();
 				int long_1=get<0>(*j);
 				int long_2=get<0>(*k);
 				int short_1=get<1>(*j);
 				int short_2=get<1>(*k);
-				c_o.push("i");
-				c_o.push("e");
-				c_c.push(short_1);
-				c_c.push(short_2);
-				c_c.push(long_1);
-				c_c.push(long_2);
-				constrain_op.push(c_o);
-				constrain_content.push(c_c);
-				set<unsigned int> c2p;
-				c2p.insert(short_1);
-				c2p.insert(short_2);
-				c2p.insert(long_1);
-				c2p.insert(long_2);
-				constrain2prefix[c_id]=c2p;
+				c.push_back(-2);				
+				c.push_back(short_1);
+				c.push_back(short_2);
+				c.push_back(-1);
+				c.push_back(long_1);
+				c.push_back(long_2);
+				constrain_content.push_back(c);
+				//constrain2prefix[c_id]={short_1,short_2,long_1,long_2};
 				prefix2constrain[short_1].insert(c_id);
 				prefix2constrain[short_2].insert(c_id);
 				prefix2constrain[long_1].insert(c_id);
 				prefix2constrain[long_2].insert(c_id);
 			}
-
-			
-
 		}
 	}
+	printf("finish condition 2\n");
 	
 	/* printing
-	for(set<int>::iterator i=dmap[1][1].begin();i!=dmap[1][1].end(); i++){
-		
-		cout<<"hi   "<<*i<<"   \n";
-	}
-	*/
+	for(set<int>::iterator i=dmap[1][1].begin();i!=dmap[1][1].end(); i++)		cout<<"hi   "<<*i<<"   \n";	*/
+	
 	//conflict graph
-	unsigned int edgeNum=0;
-	stack<unsigned int> edge_0,edge_1;
+	int edgeNum=0;
+	stack< int> edge_0,edge_1;
 	for(auto i=dmap.begin();i!=dmap.end();i++){
-		unordered_map<unsigned int,set<unsigned int>> sec = i->second;
+		unordered_map< int,set< int>> sec = i->second;
 		if(sec.find(0)!=sec.end() && sec.find(1) != sec.end()){
-			for(set<unsigned int>::iterator j=sec[0].begin(); j!=sec[0].end(); j++){
-				for(set<unsigned int>::iterator k=sec[1].begin();k!=sec[1].end();k++){
+			for(set< int>::iterator j=sec[0].begin(); j!=sec[0].end(); j++){
+				for(set< int>::iterator k=sec[1].begin();k!=sec[1].end();k++){
 					different_group[*j].insert(*k);					
 					different_group[*k].insert(*j);					
 					edge_0.push(*j);
@@ -209,7 +199,7 @@ int main(){
 			}	
 		}
 	}
-
+	cout<<"finish conflict graph output";
 	/*  printing
 	for(set<int>::iterator i=different_group[0].begin();i!=different_group[0].end(); i++){
 		
@@ -217,6 +207,7 @@ int main(){
 	}
 	*/
 	//buildin txt file for coloring
+	
 	freopen("output.txt","w",stdout);
 	ofstream dimacs;
   	dimacs.open ("search_dimacs.col");
@@ -232,69 +223,156 @@ int main(){
 	scanf("%*[^\n]\n");
 	scanf("%*[^\n]\n");
 	scanf("%*f %*s %*s %*s %d",&color); 
-	cout<<color<<"\n";
 	
+	cout<<"found lower bound number of states "<< color<<"\n";
 
-
-	
 	bool fail = true;
 	while(!fail){
 		// searching
-		unordered_map<unsigned int,set<unsigned int>> prefix_table, state2prefix;
-		//do we need state2prefix????
+		int prefix_done=0;
+		unordered_map< int,set< int>> prefix_table, state2prefix;
 		//state starts with 1!!!!!!!!!!!!!!!!!!!
-		unsigned int prefix2state[prefixes.size()];
-		stack<tuple<unsigned int,unsigned int>> prefix_table_t, state2prefix_t, prefix2state_t;
+		int prefix2state[prefixes.size()];
+		stack<tuple< int,set< int>>> prefix_table_t;
+		set<int> potential;
 		// prefix and state, state prefix, prefix state for tuples. 
-		unsigned int sb[color];
-		for(unsigned int i=1;i<prefixes.size();i++){
+		int sb[color];
+		for(int i=1;i<prefixes.size();i++){
+			//symmetry breaking
 			if(i<color+1)
 				sb[i-1]=i;
-			if(different_group[0].find(i)!=different_group[0])
-				unsigned int j=1;
+			int j;
+			//initalize
+			if(different_group[0].find(i)==different_group[0].end())
+				 j=1;
 			else
-				unsigned int j=2;
-			while(j<i+1 && j<color+1)
+				 j=2;
+			while(j<=i && j<=color)
 				prefix_table[i].insert(j++);
 		}
 		
-		prefix2state[0]=1;
-		state2prefix[1].insert(0);
-			
+		prefix2state[0]=1;	
+		prefix_done++;
+
+
+		
+
 		//search
-		symmetry_breaking(color,*sb,*prefix2state,prefix_table,prefix_table_t, false);
+		//symmetry_breaking(color,*sb,*prefix2state,prefix_table,prefix_table_t, false);
 
 		color++;
+	break;
 	}
 	cout<<"end";
 }
 
 //search: 
 //constrain_finding (most probable) via parser preper
-//	insufficient info, assumption builder and update
+//	insufficient info, assumption builder (use constrian to make assumptions) and update
 // 	sufficient info, parser
 //     all good, search
 // 	   conflict, all possible values check, conflict diagonsis, backtrack, update assumption, continue.
 //		
 
 
-void update(set<unsigned int> different_set, int state,unordered_map<unsigned int,set<unsigned int>> prefix_table, stack<tuple<unsigned int, unsigned int>> prefix_table_t){
-	for(auto i=different_set.begin();i!=different_set.end();i++){
-		prefix_table[*i].erase(state);
-		prefix_table_t.push(make_tuple(*i,state));
+set<int> set_state(int prefix,int state, int *prefix2state,unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p, stack<set<int>> prefix_table_t, bool trace){
+	*(prefix2state+prefix)=state;	
+	if(trace){
+		prefix_table_t.push(prefix_table[prefix]);
+		prefix_table_p.push(prefix);
 	}
+	prefix_table[prefix].clear();
+	prefix_table[prefix].insert(state);
 }
 
-//set<unsigned int>> constrain2prefix,unordered_map<unsigned int,set<unsigned int>> prefix2constrain){
-void symmetry_breaking(int color,(int *)sb, (int *) prefix2state,unordered_map<unsigned int,set<unsigned int>> prefix_table, stack<tuple<unsigned int,unsigned int>> prefix_table_t,bool trace){
+set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p,stack<set<int>> prefix_table_t,set<int> potential, map< int,set< int>> prefix2constrain,vector<vector<int>> constrain_content, bool trace){
+	set<int> determined;
+	int smallest=prefix_table[*(different_set.begin())].size();
+	int smallestIndex=*(different_set.begin());
+	for(auto i=different_set.begin();i!=different_set.end();i++){
+		if(prefix_table[*i].size()<smallest){
+			smallestIndex=*i;
+			smallest=prefix_table[smallestIndex].size();
+		}
+		prefix_table[*i].erase(state);
+		if(trace){
+			prefix_table_t.push({state});
+			prefix_table_p.push(*i);
+		}
+		int s = prefix_table[*i].size();
+		if(s==0)
+			return {-1};
+		else if(s==1)
+			determined.insert(s);
+	}
+	
+	set<int> p2c=prefix2constrain[prefix];
+	for(auto k=p2c.begin();k!=p2c.end();k++){
+		int constrain=*k;
+		vector<int> content=constrain_content[constrain];
+		int num_constrain = content.size()/3;
+		int undetermined=-2;
+		int result=0;
+		for(int i=0;i<num_constrain;){
+			int op=content[i++];
+			int c1=content[i++];
+			int c2=content[i++];
+			int d1= *(prefix2state+c1);
+			int d2= *(prefix2state+c2);
+			if(d1==0 || d2==0  ){
+				if(d1>0||d2>0)
+					undetermined=i-2;
+				else
+					undetermined=-1;
+				continue;
+			}
+			if(op=-1)
+				result+=(d1==d2);
+			else
+				result+=(d1!=d2);
+			if(result>0)
+				break;
+		}
+		if(result>0)
+			continue;
+		// no undetermined and no result(all false) --> conflict
+		if(undetermined==-2)
+			return {-1};
+		// exactly one undetermined
+		if(undetermined>-1 && result==num_constrain-1){
+			int op=content[undetermined++];
+			int c1=content[undetermined++];
+			int c2=content[undetermined++];
+			int d1= *(prefix2state+c1);
+			int d2= *(prefix2state+c2);
+			int p0=(d1==0)?c1:c2;
+			int s0=(d1!=0)?d1:d2;
+			if(op==-1)
+				set_state(p0,s0,prefix2state,prefix_table,prefix_table_p,prefix_table_t,trace);
+			else{
+				if(trace){
+					prefix_table_t.push({s0});
+					prefix_table_p.push(p0);
+				}	
+				
+			}
+		}
+	}
+	potential.insert(smallest);
+	return determined;
+}
+/*
+void symmetry_breaking(int color,(int *)sb, (int *) prefix2state,unordered_map< int,set< int>> prefix_table, stack<tuple< int, int>> prefix_table_t,bool trace){
 	for(int i=0;i<color;i++){
 		if( prefix2state[*(sb+i)] < i+2 && prefix2[*(sb+i)]>0){
-			for(int j=*(sb+i);j<prefix_table.size();j++){
+			
+			for(int j=++*(sb+i);j<prefix_table.size();j++){
 				int s = *(prefix_table[j].end()-1);
 				prefix_table[j].erase(s);
 				if(bool){
 					prefix_table_t.push(make_tuple(j,s));
 				}
+			
 			}
 		}
 	}
@@ -335,7 +413,7 @@ vector<int> conflict_diagnosis()
 //redo. take in tracer and relevent databases.
 void backtracking()
 
-
+*/
 	/*
 void merged(int state, int prefix, unordered_map<int,set<int>> prefix_table, bool track, stack<tuple<int,int>> prefix_table_trace){
 	if(!track){
