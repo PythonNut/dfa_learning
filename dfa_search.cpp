6ad7b74a63ffef92a58af1ff0b70dcb577d6cef6
krzxr <rongkyle@gmail.com>
2019-08-27 14:50:12 -0700

    finish update. prob works

--- dfa_search.cpp
+++ dfa_search.cpp
@@ -14,8 +14,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-set<int> set_state(int prefix,int state, int *prefix2state,unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p, stack<set<int>> prefix_table_t, bool trace);
-set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p,stack<set<int>> prefix_table_t,set<int> potential, map< int,set< int>> prefix2constrain,vector<vector<int>> constrain_content, bool trace);
+void set_state(int prefix,int state, int *prefix2state,stack<int> &p2s_t,unordered_map<int,set< int>> &prefix_table, stack<int> &prefix_table_p, stack<set<int>> &prefix_table_t, bool trace, int &finish_prefix);
+int update(int prefix,int state,int *prefix2state, stack<int> &p2s_t,unordered_map<int,set<int>> &different_group, unordered_map<int,set< int>> &prefix_table, stack<int> &prefix_table_p,stack<set<int>> &prefix_table_t, map< int,set< int>> &prefix2constrain,vector<vector<int>> &constrain_content, bool trace,int &finish_prefix);
 bool cmp(string a, string b) {
    	if(a.size()!=b.size())
 		return a.size()<b.size();
@@ -126,7 +126,7 @@ int main(){
 	int suf=0;
 	int long_pre=0;
 	for(set<string>::iterator i=prefixes.begin();i!=prefixes.end(); i++){
-		for( int j=0;j< (*i).size()+1; j++){
+		for( int j=0;j< (*i).size(); j++){
 			prefix=(*i).substr(0,j);
 			suffix=(*i).substr(j,(*i).size());
 			if(prefixes_map.find(prefix)!=prefixes_map.end()){
@@ -162,18 +162,20 @@ int main(){
 				int long_2=get<0>(*k);
 				int short_1=get<1>(*j);
 				int short_2=get<1>(*k);
-				c.push_back(-2);				
-				c.push_back(short_1);
-				c.push_back(short_2);
-				c.push_back(-1);
-				c.push_back(long_1);
-				c.push_back(long_2);
-				constrain_content.push_back(c);
-				//constrain2prefix[c_id]={short_1,short_2,long_1,long_2};
-				prefix2constrain[short_1].insert(c_id);
-				prefix2constrain[short_2].insert(c_id);
-				prefix2constrain[long_1].insert(c_id);
-				prefix2constrain[long_2].insert(c_id);
+				if(long_1!=long_2 ||short_1!=short_2){
+					c.push_back(-2);				
+					c.push_back(short_1);
+					c.push_back(short_2);
+					c.push_back(-1);
+					c.push_back(long_1);
+					c.push_back(long_2);
+					constrain_content.push_back(c);
+					//constrain2prefix[c_id]={short_1,short_2,long_1,long_2};
+					prefix2constrain[short_1].insert(c_id);
+					prefix2constrain[short_2].insert(c_id);
+					prefix2constrain[long_1].insert(c_id);
+					prefix2constrain[long_2].insert(c_id);
+				}
 			}
 		}
 	}
@@ -199,7 +201,7 @@ int main(){
 			}	
 		}
 	}
-	cout<<"finish conflict graph output";
+	cout<<"finish conflict graph output\n";
 	/*  printing
 	for(set<int>::iterator i=different_group[0].begin();i!=different_group[0].end(); i++){
 		
@@ -226,15 +228,15 @@ int main(){
 	
 	cout<<"found lower bound number of states "<< color<<"\n";
 
-	bool fail = true;
+	bool fail = false;
 	while(!fail){
 		// searching
 		int prefix_done=0;
 		unordered_map< int,set< int>> prefix_table, state2prefix;
 		//state starts with 1!!!!!!!!!!!!!!!!!!!
 		int prefix2state[prefixes.size()];
-		stack<tuple< int,set< int>>> prefix_table_t;
-		set<int> potential;
+		stack<set< int>> prefix_table_t;
+		stack<int> prefix_table_p, p2s_t;
 		// prefix and state, state prefix, prefix state for tuples. 
 		int sb[color];
 		for(int i=1;i<prefixes.size();i++){
@@ -253,15 +255,18 @@ int main(){
 		
 		prefix2state[0]=1;	
 		prefix_done++;
-
-
+		int update_index=update(0,1,prefix2state,p2s_t,different_group,prefix_table,    prefix_table_p,prefix_table_t,prefix2constrain, constrain_content,false,prefix_done);
 		
-
+		//set_state(p0,s0,prefix2state,p2s_t,prefix_table,prefix_table_p,prefix_table_t,trace, prefix_done);
+		
 		//search
 		//symmetry_breaking(color,*sb,*prefix2state,prefix_table,prefix_table_t, false);
-
+		if(prefix_done==prefixes.size()){
+			cout<<"final number of states "<<color<<"\n";
+			break;
+		}
 		color++;
-	break;
+		break;
 	}
 	cout<<"end";
 }
@@ -275,18 +280,25 @@ int main(){
 //		
 
 
-set<int> set_state(int prefix,int state, int *prefix2state,unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p, stack<set<int>> prefix_table_t, bool trace){
-	*(prefix2state+prefix)=state;	
-	if(trace){
-		prefix_table_t.push(prefix_table[prefix]);
-		prefix_table_p.push(prefix);
-	}
-	prefix_table[prefix].clear();
-	prefix_table[prefix].insert(state);
-}
+// strict deduction. should have no error. only error is in assumption
 
-set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, unordered_map<int,set< int>> prefix_table, stack<int> prefix_table_p,stack<set<int>> prefix_table_t,set<int> potential, map< int,set< int>> prefix2constrain,vector<vector<int>> constrain_content, bool trace){
-	set<int> determined;
+void set_state(int prefix,int state, int *prefix2state,stack<int> &p2s_t,unordered_map<int,set< int>> &prefix_table, stack<int> &prefix_table_p, stack<set<int>> &prefix_table_t, bool trace, int &finish_prefix){
+	*(prefix2state+prefix)=state;	
+	finish_prefix++;
+	if(prefix_table[prefix].size()>1){
+		if(trace){
+			p2s_t.push(prefix);
+			prefix_table_t.push(prefix_table[prefix]);
+			prefix_table_p.push(prefix);
+		}
+		prefix_table[prefix].clear();
+		prefix_table[prefix].insert(state);
+	}
+}
+set<int> update(int prefix,int state,int *prefix2state, stack<int> &p2s_t,unordered_map<int,set<int>> &different_group, unordered_map<int,set< int>> &prefix_table, stack<int> &prefix_table_p,stack<set<int>> &prefix_table_t, map< int,set< int>> &prefix2constrain,vector<vector<int>> &constrain_content, bool trace,int &prefix_finish){
+	set<int> different_set=different_group[prefix];
+	int update_num;
+	int update_index;
 	int smallest=prefix_table[*(different_set.begin())].size();
 	int smallestIndex=*(different_set.begin());
 	for(auto i=different_set.begin();i!=different_set.end();i++){
@@ -302,15 +314,26 @@ set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, 
 		int s = prefix_table[*i].size();
 		if(s==0)
 			return {-1};
-		else if(s==1)
-			determined.insert(s);
+		else if(s==1 && prefix2state[*i]==0){
+			set_state(*i,*(prefix_table[*i].begin()),prefix2state,p2s_t,prefix_table,prefix_table_p,prefix_table_t,trace,prefix_finish);
+			update_index=update(*i,*(prefix_table[*i].begin()),prefix2state,p2s_t,different_group,prefix_table,prefix_table_p,prefix_table_t,prefix2constrain, constrain_content,trace,prefix_finish);
+			if(update_index==-1)
+				return {-1};
+			else{
+				update_num=prefix_table[update_index].size();
+				if(update_num<smallest){
+					smallestIndex=update_index;
+					smallest=update_num;
+				}
+			}
+		}
 	}
 	
 	set<int> p2c=prefix2constrain[prefix];
 	for(auto k=p2c.begin();k!=p2c.end();k++){
 		int constrain=*k;
 		vector<int> content=constrain_content[constrain];
-		int num_constrain = content.size()/3;
+		int num_constrain = content.size();
 		int undetermined=-2;
 		int result=0;
 		for(int i=0;i<num_constrain;){
@@ -321,7 +344,7 @@ set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, 
 			int d2= *(prefix2state+c2);
 			if(d1==0 || d2==0  ){
 				if(d1>0||d2>0)
-					undetermined=i-2;
+					undetermined=i-3;
 				else
 					undetermined=-1;
 				continue;
@@ -330,16 +353,14 @@ set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, 
 				result+=(d1==d2);
 			else
 				result+=(d1!=d2);
-			if(result>0)
-				break;
 		}
-		if(result>0)
+		if(undetermined==-2 && result>0)
 			continue;
 		// no undetermined and no result(all false) --> conflict
 		if(undetermined==-2)
 			return {-1};
 		// exactly one undetermined
-		if(undetermined>-1 && result==num_constrain-1){
+		if(undetermined>-1 && result==num_constrain/3-1){
 			int op=content[undetermined++];
 			int c1=content[undetermined++];
 			int c2=content[undetermined++];
@@ -347,19 +368,34 @@ set<int> update(int prefix,int state,int *prefix2state, set<int> different_set, 
 			int d2= *(prefix2state+c2);
 			int p0=(d1==0)?c1:c2;
 			int s0=(d1!=0)?d1:d2;
-			if(op==-1)
-				set_state(p0,s0,prefix2state,prefix_table,prefix_table_p,prefix_table_t,trace);
+			if(op==-1){
+				set_state(p0,s0,prefix2state,p2s_t,prefix_table,prefix_table_p,prefix_table_t,trace,prefix_finish);
+				update_index=update(p0,s0,prefix2state,p2s_t,different_group,prefix_table,prefix_table_p,prefix_table_t,prefix2constrain, constrain_content,trace,prefix_finish);
+				if(update_index==-1)
+					return {-1};
+				else{
+					update_num=prefix_table[update_index].size();
+					if(update_num<smallest){
+						smallestIndex=update_index;
+						smallest=update_num;
+					}
+				}
+			}
 			else{
+				prefix_table[p0].erase(s0);
 				if(trace){
 					prefix_table_t.push({s0});
 					prefix_table_p.push(p0);
 				}	
-				
 			}
 		}
 	}
-	potential.insert(smallest);
-	return determined;
+	return smallestIndex;
+}
+
+
+int conflict(int prefix,int state,int *prefix2state, stack<int> &p2s_t,unordered_map<int,set<int>> &different_group, unordered_map<int,set< int>> &prefix_table, stack<int> &prefix_table_p,stack<set<int>> &prefix_table_t, map< int,set< int>> &prefix2constrain,vector<vector<int>> &constrain_content, bool trace,int &prefix_finish){
+	
 }
 /*
 void symmetry_breaking(int color,(int *)sb, (int *) prefix2state,unordered_map< int,set< int>> prefix_table, stack<tuple< int, int>> prefix_table_t,bool trace){
